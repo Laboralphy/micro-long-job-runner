69c99a2dcd0b3c7d806fbe531ceedcb14eaeeab2
MARANDET Raphael <raphael.marandet@gendarmerie.interieur.gouv.fr>
2021-03-02 13:12:59 +0100

    .env integration

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const WSS = require('@laboralphy/ws-service');
 const ServiceFront = require('./services/front');
 const ServiceMUD = require('./services/mud');
+
+require('dotenv').config();
+
 const wss = new WSS();
 wss.service(new ServiceFront());
 wss.service(new ServiceMUD());
-wss.listen(8080);
-console.log('listening on port 8080');
+wss.listen(process.env.SERVER_PORT, process.env.SERVER_ADDRESS);
+console.log('listening on port', process.env.SERVER_PORT);
